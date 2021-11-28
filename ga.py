3ce88b9c8234658ef888da58c4d4c9d556dc42c7
Rafael Cardoso <rafa_cardoso2001@hotmail.com>
2021-11-28 17:07:29 -0300

    .

---
+++ ga.py
@@ -0,0 +1,47 @@
+# Modulo GA
+
+import numpy
+
+def cal_pop_fitness(equation_inputs, pop):
+    # Calculating the fitness value of each solution in the current population. (Calculando o valor da fitness para cada solução na população atual)
+    # The fitness function calulates the sum of products between each input and its corresponding weight. (A função da fitness calcula a soma dos produtos entre cada entrada e seu peso correspondente)
+    fitness = numpy.sum(pop*equation_inputs, axis=1)
+    return fitness
+
+def select_mating_pool(pop, fitness, num_parents):
+    # Selecting the best individuals in the current generation as parents for producing the offspring of the next generation. (Selecionando os melhores indivíduos na geração atual como parentes para produzir o offspring da próxima geração)
+    parents = numpy.empty((num_parents, pop.shape[1]))
+    for parent_num in range(num_parents):
+        max_fitness_idx = numpy.where(fitness == numpy.max(fitness))
+        max_fitness_idx = max_fitness_idx[0][0]
+        parents[parent_num, :] = pop[max_fitness_idx, :]
+        fitness[max_fitness_idx] = -99999999999
+    return parents
+
+def crossover(parents, offspring_size):
+    offspring = numpy.empty(offspring_size)
+    # The point at which crossover takes place between two parents. Usually, it is at the center. (O ponto em que qual crossover toma lugar entre dois parentes. Geralmente, é no centro)
+    crossover_point = numpy.uint8(offspring_size[1]/2)
+
+    for k in range(offspring_size[0]):
+        # Index of the first parent to mate. (Index do primeiro parente a acasalar)
+        parent1_idx = k%parents.shape[0]
+        # Index of the second parent to mate. (Index do segundo parente a acasalar)
+        parent2_idx = (k+1)%parents.shape[0]
+        # The new offspring will have its first half of its genes taken from the first parent. (O novo offspring vai ter a primeira metade dos genes tomada do primeiro parente)
+        offspring[k, 0:crossover_point] = parents[parent1_idx, 0:crossover_point]
+        # The new offspring will have its second half of its genes taken from the second parent. (O novo offspring vai ter a segunda metade dos genes tomada do primeiro parente)
+        offspring[k, crossover_point:] = parents[parent2_idx, crossover_point:]
+    return offspring
+
+def mutation(offspring_crossover, num_mutations=1):
+    mutations_counter = numpy.uint8(offspring_crossover.shape[1] / num_mutations)
+    # Mutation changes a number of genes as defined by the num_mutations argument. The changes are random. (Mutação troca o número de genes definidos pelo argumento num_mutations)
+    for idx in range(offspring_crossover.shape[0]):
+        gene_idx = mutations_counter - 1
+        for mutation_num in range(num_mutations):
+            # The random value to be added to the gene. (Um valor aleatório vai ser adicionado no gene)
+            random_value = numpy.random.uniform(-1.0, 1.0, 1)
+            offspring_crossover[idx, gene_idx] = offspring_crossover[idx, gene_idx] + random_value
+            gene_idx = gene_idx + mutations_counter
+    return offspring_crossover
---
+++ main.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as mpl
+import ga
+# Equação escolhida
+# Y = w1x1 + w2x2 + w3x3 + w4x4 + w5x5 + w6x6
+# (x1,x2,x3,x4,x5,x6) = (4,-2,3.5,5,-11,-4.7)
+# A equação possui 6 inputs e 6 pesos
+
+# Entradas da equação
+entradas_eq = [4,-2,3.5,5,-11,-4.7] # [4,-2,3.5,5,-11]
+
+# Número de pesos
+pesosQtd = len(entradas_eq) # Nesse caso 5 inputs
+
+# Solução por pop
+solPop = 8
+
+num_parents_mating = 4
+
+# Definindo o tamanho da população, pop terá solPop cromossomo que tera gene pesosQtd
+popTam = (solPop, pesosQtd)
+
+# Criando a pop inicial de forma aleatória
+novaPop = np.random.uniform(low=-4.0, high=4.0, size=popTam)
+print(novaPop)
+
+melhoresSaidas = []
+numGeracoes = 5
+
+for generation in range(numGeracoes):
+    print("Geração : ", generation)
+
+    # Medir a fitness de cada cromossomo na população
+    fitness = ga.cal_pop_fitness(entradas_eq, novaPop)
+    print("Fitness")
+    print(fitness)
+
+    melhoresSaidas.append(np.max(np.sum(novaPop*entradas_eq, axis=1)))
+    # O melhor resultado na iteração atual
+    print("Best result : ", np.max(np.sum(novaPop*entradas_eq, axis=1)))
+
+    # Seleção dos melhores parentes da população para o acasalamento
+    parents = ga.select_mating_pool(novaPop, fitness, num_parents_mating)
+
+    # Gerando próxima geração usando crossover
+    offspring_crossover = ga.crossover(parents, offspring_size=(novaPop[0]-parents.shape[0], pesosQtd))
+    print("Crossover")
+    print(offspring_crossover)
+
+    # Adicionando algumas variações ao offspring usando mutação
+    offspring_mutation = ga.mutation(offspring_crossover)
+    print("Mutation")
+    print(offspring_mutation)
+
+    # Criar a nova população com base nos parentes e offspring
+    novaPop[0:parents.shape[0], :] = parents
+    novaPop[parents.shape[0]:, :] = offspring_mutation
+
+# Obtendo a melhor solução após a iteração finalizando todas as gerações.
+# Primeiro, a primeira fitness é calculada para cada solução na geração final
+fitness = ga.cal_pop_fitness(entradas_eq, novaPop)
+
+# O retorno do index da solução correspondendo ao melhor fitness
+melhorIdx = np.where(fitness == np.max(fitness))
+
+print("Best solution : ", novaPop[melhorIdx, :])
+print("Best solution fitness : ", fitness[melhorIdx])
+
+mpl.plot(melhoresSaidas)
+mpl.xlabel("Iteration")
+mpl.ylabel("Fitness")
+mpl.show()
